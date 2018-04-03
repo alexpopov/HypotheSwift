8899248d3ecd0c3a37b7a49d99d0d3e50f2c199e
Alex Popov <alex.popov@rakuten.com>
2018-04-03 10:54:03 -0700

    Initial Commit

---
+++ Sources/HypotheSwift/HypotheSwift.h
@@ -0,0 +1,19 @@
+//
+//  HypotheSwift.h
+//  HypotheSwift
+//
+//  Created by John Doe on 02/07/2017.
+//  Copyright © 2017 John Do. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HypotheSwift.
+FOUNDATION_EXPORT double HypotheSwift_VersionNumber;
+
+//! Project version string for HypotheSwift.
+FOUNDATION_EXPORT const unsigned char HypotheSwift_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HypotheSwift_/PublicHeader.h>
+
+
